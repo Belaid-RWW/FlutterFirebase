3b36d8b1a4de0bfddb110e8d3d6f030fc840cc1f
Fedy Belaid <fedybelaid1902@gmail.com>
2022-02-01 21:09:35 +0100

    Custom user model update

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_firebase/models/my_user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //create user object based on firebase user
+  MyUser? _userFromFirebaseUser(User user) {
+    return user != null ? MyUser(uid: user.uid) : null;
+  }
 
   //anonymous sign in
   Future singInAnon() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
-      return user;
+      return _userFromFirebaseUser(user!);
     } catch (e) {
       print(e.toString());
       return null;
