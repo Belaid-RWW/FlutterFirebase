f999e486ad277b78fe6571c75ce622d15fdcf2a3
Fedy Belaid <fedybelaid1902@gmail.com>
2022-02-02 23:31:24 +0100

    Sign in with firebase

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -11,9 +11,11 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
+  final _formkey = GlobalKey<FormState>();
 
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -33,16 +35,20 @@ class _SignInState extends State<SignIn> {
       body: Container(
           padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 15.0),
           child: Form(
+            key: _formkey,
             child: Column(
               children: <Widget>[
                 SizedBox(height: 20.0),
                 TextFormField(
+                  validator: (val) => val!.isEmpty ? 'Enter an email' : null,
                   onChanged: (val) {
                     setState(() => email = val);
                   },
                 ),
                 SizedBox(height: 20.0),
                 TextFormField(
+                  validator: (val) =>
+                      val!.length < 6 ? 'Enter a 6+ chars password' : null,
                   obscureText: true,
                   onChanged: (val) {
                     setState(() => password = val);
@@ -50,11 +56,23 @@ class _SignInState extends State<SignIn> {
                 ),
                 SizedBox(height: 20.0),
                 ElevatedButton(
-                  child: Text('Log in', style: TextStyle(color: Colors.white)),
-                  onPressed: () async {
-                    print(email);
-                    print(password);
-                  },
+                    child:
+                        Text('Log in', style: TextStyle(color: Colors.white)),
+                    onPressed: () async {
+                      if (_formkey.currentState!.validate()) {
+                        dynamic result = await _auth.signInWithEmailAndPassword(
+                            email, password);
+                        if (result == null) {
+                          setState(() => error = 'Verify your credentials');
+                        }
+                      }
+                    }),
+                SizedBox(
+                  height: 12.0,
+                ),
+                Text(
+                  error,
+                  style: TextStyle(color: Colors.red, fontSize: 14.0),
                 )
               ],
             ),
